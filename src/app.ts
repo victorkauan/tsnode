abf735db5bd14ad96d5f878fd85a1f87cb8e340b
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2021-12-13 18:28:19 -0300

    feat: create a simple application using classes

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+class App {
+    public express : express.Application
+
+    public constructor () {
+      this.express = express()
+
+      this.middlewares()
+      this.database()
+      this.routes()
+    }
+
+    private middlewares (): void {
+      this.express.use(express.json())
+      this.express.use(cors())
+    }
+
+    private database (): void {
+      mongoose.connect('mongodb://localhost:27017/tsnode')
+    }
+
+    private routes (): void {
+      this.express.get('/', (req, res) => {
+        return res.send('Hello, world!')
+      })
+    }
+}
+
+export default new App().express
