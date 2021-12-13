8ee32ba9772308125b5d6753d94b353ff91bee25
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2021-12-13 19:25:48 -0300

    feat: user creation and listing routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import routes from './routes'
+
 class App {
     public express : express.Application
 
@@ -23,9 +25,7 @@ class App {
     }
 
     private routes (): void {
-      this.express.get('/', (req, res) => {
-        return res.send('Hello, world!')
-      })
+      this.express.use(routes)
     }
 }
 
